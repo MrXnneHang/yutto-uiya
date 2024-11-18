9617e067d2f977c2ce3b0ceb0c73b0f5c1e6c7d9
MrXnneHang <XnneHang@gmail.com>
2024-11-18 22:04:45 +0800

    :poop: bad code: The `-p` args can't work now.

--- yutto/__init__.py
+++ yutto/__init__.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
-from .user_video import user_single_video
+from .user_video import (
+    user_multi_video,
+    user_single_video,
+)
 
 __all__ = [
     "user_single_video",
--- yutto/user_video.py
+++ yutto/user_video.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from utils.subproc import run_command
 
 
-def user_single_video(url: str, args: list, SESSDATA: str = None) -> int:
+def user_single_video(url: str, args: list = None, SESSDATA: str = None) -> int:
     """
     下载单个视频
     :param url: 视频链接
@@ -12,6 +12,38 @@ def user_single_video(url: str, args: list, SESSDATA: str = None) -> int:
     :return: 200/404/500
     """
     command = ["yutto", url]
+    if SESSDATA:
+        command.extend(["--sessdata", SESSDATA])
+    if args:
+        command.extend(args)  # 以空格分割额外参数
+
+    try:
+        run_command(command)
+    except Exception as e:
+        return 404
+    return 200
+
+
+# TODO: 似乎这个p参数不能指定要下载的视频，只会默认下载第一个视频
+def user_multi_video(
+    urls: str, p: str = "", args: list = None, SESSDATA: str = None
+) -> int:
+    """
+    下载一个视频列表下的多个视频
+    示例: https://www.bilibili.com/video/BV1vZ4y1M7mQ
+    :param urls: 视频链接
+    :param p: 指定要下载哪些p("1,2","1~2",start 1, end -1)
+    :param args: 额外输入参数
+    :param SESSDATA: SESSDATA, 用于保持用户登录信息
+    :return: 200/404/500
+    """
+    if p:
+        # 指定下载
+        command = ["yutto", urls, "-p", p]
+    else:
+        # 全部下载
+        command = ["yutto", urls, "-p", "1~-1"]
+
     if SESSDATA:
         command.extend(["--sessdata", SESSDATA])
     if args:
