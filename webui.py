8375586e1c6dd426c7911a9c781414e4a2dd7f46
MrXnneHang <XnneHang@gmail.com>
2024-11-19 09:16:37 +0800

    :white_check_mark: test: Test for User Collections.

--- webui.py
+++ webui.py
@@ -1,16 +1,24 @@
 from __future__ import annotations
 
 from utils.config import generate_basic_args
-from yutto import user_multi_video, user_single_video
+from yutto import user_collection_video, user_multi_video, user_single_video
 
 if __name__ == "__main__":
     args = generate_basic_args()
+    # ----------------------
     # user_single_video(
     #     url="https://www.bilibili.com/video/BV1vZ4y1M7mQ/",
     #     args=args,
     # )
-    user_multi_video(
-        urls="https://www.bilibili.com/video/BV1vZ4y1M7mQ/",
-        p="2",
+
+    # ----------------------
+    # user_multi_video(
+    #     urls="https://www.bilibili.com/video/BV1vZ4y1M7mQ/",
+    #     p="2",
+    #     args=args,
+    # )
+
+    user_collection_video(
+        url="https://space.bilibili.com/100969474/channel/seriesdetail?sid=1947439",
         args=args,
     )
