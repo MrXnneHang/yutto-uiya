c28effbdab23e17fed2f0762517abab19e3aab94
MrXnneHang <XnneHang@gmail.com>
2024-11-19 11:14:33 +0800

    :poop: bad code: `bangumi_single_episode` need to check.

--- webui.py
+++ webui.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from utils.config import generate_basic_args
 from yutto import (
+    bangumi_single_episode,
+    bangumi_single_season,
     user_collection_video,
     user_multi_favor_list,
     user_multi_video,
@@ -43,10 +45,24 @@ if __name__ == "__main__":
     #     args=args,
     # )
 
-    # TODO:Failed
+    # TODO: Failed
     # ----------------------
     # user_space_video(
     #     url="https://space.bilibili.com/100969474/video",
     #     args=args,
     #     page="1",
+    # )
+
+    # TODO: 功能和想象的不太一样，不是获取指定话，只能批量，然后选集。
+    # ----------------------
+    # bangumi_single_episode(
+    #     url="https://www.bilibili.com/bangumi/play/ss48811",
+    #     args=args,
+    # )
+
+    # ----------------------
+    # bangumi_single_season(
+    #     url="https://www.bilibili.com/bangumi/media/md23053814",
+    #     p="2",
+    #     args=args,
     # )
